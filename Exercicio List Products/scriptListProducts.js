cf1c283acb861538478dadd2267e75c86e8a8b79
RicardoPires04 <ricardomanuelfpires@gmail.com>
2023-07-08 21:48:27 +0100

    Atualizei o codigo de listar produtos

--- Exercicio List Products/scriptListProducts.js
+++ Exercicio List Products/scriptListProducts.js
@@ -1,6 +1,25 @@
-var produtos = new Array(nome, categoria, preco);
+var produtos = new Array (
+    {nome: "Nike", categoria: "T-Shirt", preco: 35},
+    {nome: "Adidas", categoria: "Shoes", preco: 100},
+    {nome: "Puma", categoria: "Cargo Pants", preco: 65}
+);
 
-produtos(0) = new Array("Nike", "T-Shirt", 35);
-produtos(1) = new Array("Adidas", "Shoes", 100);
-produtos(2) = new Array("Puma", "Cargo Pants", 65);
 
+function listaTabela(){
+    let tbody = document.getElementById("tbody");
+
+    for (let i = 0; i < this.produtos.length; i++){
+        let tr = tbody.insertRow();
+
+        let td_nome = tr.insertCell();
+        let td_categoria = tr.insertCell();
+        let td_preco = tr.insertCell();
+
+        td_nome.innerText = this.produtos[i].nome;
+        td_categoria.innerText = this.produtos[i].categoria;
+        td_preco.innerText = this.produtos[i].preco;
+
+    }
+
+    this.listaTabela();
+}
